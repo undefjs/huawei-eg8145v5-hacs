e5cc4e69efb3e18ca6f212ed0da3164974d973e6
undefjs <davidibarrajerez@gmail.com>
2025-11-20 22:17:25 +0100

    Fix: Simplify config - remove SSL options, fix constants conflict

--- custom_components/huawei_eg8145v5/const.py
+++ custom_components/huawei_eg8145v5/const.py
@@ -4,12 +4,5 @@ from typing import Final
 
 DOMAIN: Final = "huawei_eg8145v5"
 
-CONF_HOST: Final = "host"
-CONF_USERNAME: Final = "username"
-CONF_PASSWORD: Final = "password"
-CONF_SSL: Final = "ssl"
-CONF_VERIFY_SSL: Final = "verify_ssl"
-
+# Configuration defaults
 DEFAULT_USERNAME: Final = "Epuser"
-DEFAULT_SSL: Final = False
-DEFAULT_VERIFY_SSL: Final = False
